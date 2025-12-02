3150ed30d9f0d263e84d038d5942fa885947d7f1
mahmoud-saleh145 <salehmahmoud327@gmail.com>
2025-12-02 16:40:32 +0200

    add front url cors semifainal fix

--- index.js
+++ index.js
@@ -24,9 +24,10 @@ app.use(cookieParser());
 app.use(cors({
     origin: [
         "http://localhost:3000",
-        process.env.FRONTEND_URL,
         "https://nasiej-2.vercel.app",
-        "https://nasiej-2-mahmouds-projects-fc34b8b3.vercel.app"
+        "https://nasiej-2-mahmouds-projects-fc34b8b3.vercel.app",
+        "https://nasiej-2-git-main-mahmouds-projects-fc34b8b3.vercel.app",
+        "https://nasiej-2-4lpq8tf0i-mahmouds-projects-fc34b8b3.vercel.app"
     ],
     credentials: true,
 }));
